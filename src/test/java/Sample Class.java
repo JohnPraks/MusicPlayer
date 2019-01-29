6fe5b5d743774a5acb1b8de3e127caf2c6bd8596
Prakash Dontha <PrakashDontha.Dontha@wnco.com>
2019-01-29 20:45:39 +0530

    Testing the revert command

---
+++ src/test/java/Sample Class.java
@@ -0,0 +1,12 @@
+package com.rythmos.demo;
+
+/** 
+* @author Prakash Dontha
+* this is general class to test JavaDoc Tool
+* 
+*/
+public class SampleClass {
+	public static void main(String ars[]){
+		System.out.println("this is sample class to test the javadoc tool");
+	}
+}
